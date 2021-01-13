2b0e625288723b6a8b153f95b39f5e420d2ff863
Carsten Knoll <carsten.knoll@posteo.de>
2021-01-13 20:32:47 +0100

    automatically generate property facts

--- ontology/create_ontology.py
+++ ontology/create_ontology.py
@@ -31,6 +31,14 @@ def write_txt(path, txt):
         f.write(txt)
 
 
+# noinspection PyShadowingNames
+def get_input_types(raw_dict: dict) -> list:
+    res = []
+    for key, inner_dict in raw_dict.items():
+        res.append(inner_dict["type"])
+    return res
+
+
 onto_src_part1 = load_txt(base_ontology_path)
 
 mn_types_methods_yaml_dict = load_yaml("../new_types.yml")
@@ -46,18 +54,36 @@ separator = {"annotation": f"{nl*1}{'#'*5}{nl*1}"}
 
 onto_list = []
 
+# temporarily store information about inputs and outputs
+input_facts = []
+output_facts = []
+
 MN_Type = "TypeEntity"
 MN_Method = "MethodEntity"
 
+# handle types
 for mn_type, inner_dict in types_dict.items():
     onto_list.append({"owl_individual":
                           {f"iMN_{mn_type}": {"types": [MN_Type]}}})
 
 onto_list.append(separator)
 
+# handle methods
 for mn_method, inner_dict in methods_dict.items():
+
+    idv_name = f"iMN_{mn_method}"
+
     onto_list.append({"owl_individual":
-                          {f"iMN_{mn_method}": {"types": [MN_Method]}}})
+                          {idv_name: {"types": [MN_Method]}}})
+
+    input_dict = inner_dict["inputs"]
+    outputs_dict = inner_dict["outputs"]
+
+    input_types = get_input_types(input_dict)
+    for it in input_types:
+        input_facts.append({idv_name: f"iMN_{it}"})
+
+onto_list.append({"property_facts": {"hasForInput": {"Facts": input_facts}}})
 
 
 onto_src_part2 = yaml.dump(onto_list, allow_unicode=True)
@@ -67,4 +93,3 @@ write_txt(result_path, f"{onto_src_part1}{separator['annotation']}{onto_src_part
 
 om = ypo.OntologyManager(result_path)
 
-IPS()
