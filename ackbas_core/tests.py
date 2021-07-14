d0e3aba205c936f8e3f49da50db75ee0d7481fcf
Robert Heedt <robert.heedt@gmail.com>
2021-07-14 11:55:50 +0200

    Test solution sketch algorithm

--- ackbas_core/tests.py
+++ ackbas_core/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 from ackbas_core.knowledge_graph import RTGraph, RTEnumType, RTParamPlaceholder, RTParamUnset, RTEnumValue
+from ackbas_core.solution_sketch import RTObjectInstance
+from ackbas_core.views import GetSolutionGraphView
 
 
 class KnowledgeGraphTest(TestCase):
@@ -24,3 +26,35 @@ class KnowledgeGraphTest(TestCase):
         self.assertIsInstance(graph.methods["TestProperty"].inputs["objectTwo"].param_constraints["ValueEnum"], RTParamUnset)
         self.assertIsInstance(graph.methods["TestProperty"].outputs["optionGood"]["objectTwo"].param_statements["ValueEnum"], RTEnumValue)
         self.assertEqual(graph.methods["TestProperty"].outputs["optionGood"]["objectTwo"].param_statements["ValueEnum"].val, 0)
+
+
+class SolutionSketchTest(TestCase):
+    def test_solution(self):
+        start_dict = {
+            "start": {
+                "type": "TypeOne",
+                "params": {
+                    "ValueOne": "123456"
+                }
+            }
+        }
+
+        target_dict = {
+            "target": {
+                "type": "TypeThree"
+            }
+        }
+
+        sol = GetSolutionGraphView.get_solution("minimal", start_dict, target_dict)
+
+        def constructable_from_start_objects(obj_id):
+            obj = [obj for obj in sol["objects"] if obj["id"] == obj_id][0]
+            if obj["is_start"]:
+                return True
+
+            preceeding_method_id = [con["fromId"] for con in sol["connections"] if con["toId"] == obj_id][0]
+            preceeding_object_ids = [con["fromId"] for con in sol["connections"] if con["toId"] == preceeding_method_id]
+
+            return all(constructable_from_start_objects(other_obj_id) for other_obj_id in preceeding_object_ids)
+
+        self.assertTrue(all(constructable_from_start_objects(obj["id"]) for obj in sol["objects"] if obj["is_end"]))
